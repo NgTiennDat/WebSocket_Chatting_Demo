b4f125ad06982e71fc170d6be73a3cd4acd93e38
NgTiennDat <126660251+NgTiennDat@users.noreply.github.com>
2024-06-29 17:03:49 +0700

    app done

---
+++ src/main/java/com/datien/websocket_chat/controller/ChatController.java
@@ -0,0 +1,28 @@
+package com.datien.websocket_chat.controller;
+
+import com.datien.websocket_chat.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUSer(
+            @Payload ChatMessage chatMessage,
+            SimpMessageHeaderAccessor headerAccessor
+    ) {
+        // add username in websocket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+}
